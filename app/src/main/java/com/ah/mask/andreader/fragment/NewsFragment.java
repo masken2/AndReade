b4b4beea97b40c2f01bef0aa21b546323d0fea09
zhuangAH <zhuangmask123@163.com>
2015-11-26 18:34:35 +0800

    1.增加界面

---
+++ app/src/main/java/com/ah/mask/andreader/fragment/NewsFragment.java
@@ -0,0 +1,26 @@
+package com.ah.mask.andreader.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.ah.mask.andreader.BaseFragment;
+
+/**
+ * 新闻界面
+ * Created by zhuangAh on 2015/11/26.
+ */
+public class NewsFragment extends BaseFragment {
+
+    private View view;
+    @Override
+    protected View initView(LayoutInflater inflater) {
+//        inflater.inflate(R.layout.fragment_friends);
+        return null;
+    }
+
+    @Override
+    protected void initDate(Bundle savedInstanceState) {
+
+    }
+}
---
+++ app/src/main/java/com/ah/mask/andreader/fragment/SettingFragment.java
@@ -0,0 +1,27 @@
+package com.ah.mask.andreader.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.ah.mask.andreader.BaseFragment;
+import com.ah.mask.andreader.R;
+
+/**
+ * 新闻界面
+ * Created by zhuangAh on 2015/11/26.
+ */
+public class SettingFragment extends BaseFragment {
+
+    private View view;
+    @Override
+    protected View initView(LayoutInflater inflater) {
+        view =inflater.inflate(R.layout.fragment_news,null);
+        return null;
+    }
+
+    @Override
+    protected void initDate(Bundle savedInstanceState) {
+
+    }
+}
